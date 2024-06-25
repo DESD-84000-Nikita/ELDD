698a681234e6bbfaebe29560bc1ad309ed1ce46b
sunbeam <nikitakusnurendk01@gmail.cm>
2024-06-25 21:27:21 +0530

    keyboard

--- BBB _Makefile/Makefile_BBB.c
+++ BBB _Makefile/Makefile_BBB.c
@@ -2,7 +2,7 @@
 obj-m = pchar.o
 
 modules:
-	make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- -C /home/ghanshyam/linux M=`pwd` modules 
+	make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- -C /home/sunbeam/Desktop/linux/ M=`pwd` modules 
 
 clean:
 	make -C /lib/modules/`uname -r`/build M=`pwd` clean
